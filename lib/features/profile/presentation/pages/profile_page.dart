77cdefe3f60b09c6f372b49391ce09375a70f8e4
plushevyj <ischencko.kir@gmail.com>
2026-06-30 04:48:07 +0300

    cosmetic

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -14,6 +14,7 @@ class _ProfilePageState extends State<ProfilePage> {
   
   @override
   Widget build(BuildContext context) {
+    print('state: ${context.read<AuthBloc>().state}');
     return BlocBuilder<AuthBloc, AuthState>(
       builder: (context, state) {
         if (state is! AuthAuthenticated) {
